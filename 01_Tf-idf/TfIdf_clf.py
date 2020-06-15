3016c4ee442f01a5cdeb4b09b4ab47d1ce841501
张育博 <zyb.mp3@gmail.com>
2020-06-15 12:49:21 +0800

    add bert code

--- 01_Tf-idf/TfIdf_clf.py
+++ 01_Tf-idf/TfIdf_clf.py
@@ -8,16 +8,23 @@ class TfidfClassifier(object):
         self.ngram = ngram
         self.vectorizer = TfidfVectorizer(analyzer=self.token_level, token_pattern="(?u)\b\w+\b", min_df=min_df)
         self.classifier = SVC(kernel='linear')
+        self.has_trained = False
 
 
-    def train(self, text, label):
+    def train(self, texts, labels):
         """
         模型训练
-        :param text:
-        :param label:
+        :param texts:
+        :param labels:
         :return:
         """
         # 1. 训练vectorizer
-        self.vectorizer.fit(text)
-        train_X = self.vectorizer.transform(text)
-        self.classifier.fit(train_X, label)
+        self.vectorizer.fit(texts)
+        train_X = self.vectorizer.transform(texts)
+        self.classifier.fit(train_X, labels)
+        self.has_trained = True
+
+    def predict_single(self, text: str):
+        text_vector = self.vectorizer.transform(text)
+        return self.classifier.predict(text_vector)
+
---
+++ 01_Tf-idf/run_tfidf.py
@@ -0,0 +1,1 @@
+import TfIdf_clf
