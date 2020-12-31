02c868f961fc61a6c24b866f68681226b326ca6a
zhangyubo15 <zhangyubo15@ZBMAC-C02DN1JXM.local>
2020-12-31 11:59:26 +0800

    create dir

---
+++ 01_Fasttext/model.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import tensorflow.python.keras.models as models
+import tensorflow.python.keras.layers as layers
+
+class Fasttext(object):
+
+    def __init__(self, vocab_size, embedding_size, max_sentence_len):
+        self.vocab_size = vocab_size
+        self.embedding_size = embedding_size
+        # create model
+        self.model = models.Sequential()
+        self.model.add(layers.Embedding(vocab_size, embedding_size, max_sentence_len))
+        self.model.add(layers.GlobalAveragePooling1D())
+        self.model.add(layers.Dense(units=1, activation='sigmoid'))
+        # 损失和优化器选择
+        self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    def
